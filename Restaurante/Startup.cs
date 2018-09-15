4bbd79dc3ea2eb213d6f6a25164828f6be8c2d5e
kumanyai <wyrez26@gmail.com>
2018-09-14 20:41:38 -0600

    Iniciando Aplicacion

---
+++ Restaurante/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Restaurante
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //PONDREMOS NUESTROS INTERFACES/SERVICIOS PERSONALIZADOS
+            //InvalidOperationException: No service for type 'Restaurante.IGreeter' has been registered.
+            services.AddSingleton<IGreeter, Greeter>();//CREAMOS LA INSTANCIA DE LA CLASE IGreeter
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app,
+                              IHostingEnvironment env,
+                              IGreeter Greeter)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                var greeting = Greeter.GetMessageOfTheDay();
+                await context.Response.WriteAsync(greeting);
+            });
+        }
+    }
+}
