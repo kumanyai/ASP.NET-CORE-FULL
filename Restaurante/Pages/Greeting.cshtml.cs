48d4440f3fe2099a179ba2e64361c37b7fe6b31f
kumanyai <wyrez26@gmail.com>
2018-09-16 21:26:54 -0600

    Pendiente

---
+++ Restaurante/Pages/Greeting.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Resturante.Services;
+
+namespace Restaurante.Pages
+{
+    public class GreetingModel : PageModel
+    {
+        private IGreeter _greeter;
+
+        public GreetingModel(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
